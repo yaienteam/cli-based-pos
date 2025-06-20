f9f15525665d92612854408ce0a7d66d52eb3eb7
ijlal tauqir <yaienyaienstore@gmail.com>
2025-06-20 15:08:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+
+def manage_products():
+    print("Managing Products")
+def add_product():
+    print("Adding Product")
+
+
+def view_products():
+    print("Viewing Products")
+
+def update_or_delete_product():
+    print("Update/Delete Product")
+
+
+def main():
+    while True:
+
+        start = input("Press 'Enter key' to start the POS System: ")
+        if start == "":
+            break
+        else:
+            print("Press Enter to start.")
+
+
+    while True:
+        print(" Main Menu — select an option from the following:")
+        print("1. Manage Products")
+        print("2-Add Product")
+        print("3 View Product")
+        print("4.   (Reserved for Future Feature)")
+        print("5. Update/Delete Product")
+
+        choice = input("Enter your choice (1-5): ")
+
+
+
+
+
+        if choice == '1':
+            manage_products()
+        elif choice == '2':
+            add_product()
+        elif choice == '3':
+            view_products()
+        elif choice == '5':
+            update_or_delete_product()
+        else:
+            print(" Please select a option from these to start  (1-5).")
+
+if __name__ == "__main__":
+    main()
