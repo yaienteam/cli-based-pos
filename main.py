752fdbe54203f62783ee41a91b05ff5eb3d7a641
sharyarnaveed <sharyarmalik430@gmail.com>
2025-06-21 16:20:22 +0500

    sign in added

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ def view_products():
 def update_or_delete_product():
     print("Update/Delete Product")
 
+def signin():
+    print("In Sign in")
+    
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    with open('password.txt','r') as file:
+        for line in file:
+            fileusername,filepassword=line.strip().split(',')
+            if username==fileusername and password==filepassword:
+                return True
+            return False
+    
 
 def main():
     while True:
@@ -21,31 +34,36 @@ def main():
         else:
             print("Press Enter to start.")
 
+    if signin():
+         print("Login successful!")
+         while True:
+            print(" Main Menu — select an option from the following:")
+            print("1. Manage Products")
+            print("2-Add Product")
+            print("3 View Product")
+            print("4.   (Reserved for Future Feature)")
+            print("5. Update/Delete Product")
 
-    while True:
-        print(" Main Menu — select an option from the following:")
-        print("1. Manage Products")
-        print("2-Add Product")
-        print("3 View Product")
-        print("4.   (Reserved for Future Feature)")
-        print("5. Update/Delete Product")
-
-        choice = input("Enter your choice (1-5): ")
-
-
-
+            choice = input("Enter your choice (1-5): ")
 
 
-        if choice == '1':
-            manage_products()
-        elif choice == '2':
-            add_product()
-        elif choice == '3':
-            view_products()
-        elif choice == '5':
-            update_or_delete_product()
-        else:
-            print(" Please select a option from these to start  (1-5).")
+
+
+
+            if choice == '1':
+             manage_products()
+            elif choice == '2':
+                add_product()
+            elif choice == '3':
+             view_products()
+            elif choice == '5':
+             update_or_delete_product()
+            else:
+             print(" Please select a option from these to start  (1-5).")
+    else:
+        print("INVALID USERNAME PASSWORD")
+
+
 
 if __name__ == "__main__":
     main()
